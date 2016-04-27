2f49398d02f7e761a4cfb1d3886ad8630c0e40b3
Tyler Filla <tylerfilla@gmail.com>
2016-04-27 16:00:02 -0500

    Cleanup and custom editor

--- app/src/main/java/io/microdev/source/EditActivity.java
+++ app/src/main/java/io/microdev/source/EditActivity.java
@@ -8,6 +8,9 @@ public class EditActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Show layout
         setContentView(R.layout.activity_edit);
     }
+
 }
---
+++ app/src/main/java/io/microdev/source/widget/SourceEditor.java
@@ -0,0 +1,13 @@
+package io.microdev.source.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class SourceEditor extends EditText {
+
+    public SourceEditor(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
